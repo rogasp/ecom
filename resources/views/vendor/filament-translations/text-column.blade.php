b6a27b17ecb1b462c7191be9f4be8da051a672bf
Roger Aspein <raspelin69@gmail.com>
2024-07-20 17:18:44 +0200

    fix vor filament-translation GPT

--- resources/views/vendor/filament-translations/text-column.blade.php
+++ resources/views/vendor/filament-translations/text-column.blade.php
@@ -1,17 +1,10 @@
 <div class="my-4">
     @if(is_array($getState()))
         <div>
-            @foreach($getState() as $key => $item)
-                <div class="flex justify-start gap-4 my-2">
+            @foreach($getState() as $key=>$item)
+                <div class="flex justifiy-start gap-4 my-2">
                     <div class="border dark:border-gray-700 rounded-full" style="padding-left: 10px; padding-right: 10px">
-                        {{--  Refactored code to get locale label  --}}
-                            <?php
-                            $localeLabel = collect(config('filament-translations.locals'))->first(function ($locale) use ($key) {
-                                return $locale['flag'] === $key;
-                            })['label'] ?? $key;
-
-                            echo e($localeLabel);
-                            ?>
+                        {{ config('filament-translations.locals')[$key]['label'] }}
                     </div>
                     <div>{{ $item }}</div>
                 </div>
