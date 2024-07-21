b1df2efbce5fbab744ce64e62a988085fefc8ffb
Roger Aspein <raspelin69@gmail.com>
2024-07-21 13:58:00 +0200

    position for shopingcart in menu

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -32,6 +32,18 @@
 
                 <!-- Navigation Links (Sidebar) -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    @guest()
+                        <a
+                            wire:navigate
+                            href="{{route('cart')}}"
+                            class="inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                            </svg>
+
+                            ({{ $cart->getItemsCount() }})
+                        </a>
+                    @endguest
                     @foreach($navigationItemsSidebar as $item)
                         @if($item['show_for'] === 'public' && !auth()->user())
                             <x-nav-link href="{{ route($item['url']) }}" :active="request()->routeIs($item['url'])">
@@ -47,19 +59,6 @@
                             </x-nav-link>
                         @endif
                     @endforeach
-
-                    @guest()
-                        <a
-                            wire:navigate
-                            href="{{route('cart')}}"
-                            class="inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                            </svg>
-
-                            ({{ $cart->getItemsCount() }})
-                        </a>
-                    @endguest
                 </div>
 
             @auth
